a4c28575910e61c2bf0d44171a7ed263097f0ab3
Brian Chang <bchang85@hotmail.com>
2015-11-15 13:59:24 -0800

    Remove the need to click a render button

--- src/data.js
+++ src/data.js
@@ -1,6 +1,8 @@
 window.data = [];
 
 var parseDate = d3.time.format("%Y%m%d").parse;
+var processCount = 0;
+var fileCount = 0;
 
 var processFileContents = function (content) {
     // underscore might be helpful here?
@@ -32,11 +34,84 @@ var processFileContents = function (content) {
             }
         );
     }
+
+    processCount++;
+    renderStepChart();
 };
+
+var renderStepChart = function renderStepChart() {
+    if (processCount != fileCount) {
+        return;
+    }
+
+    var margin = {top: 20, right: 20, bottom: 30, left: 50};
+    var width = 960 - margin.left - margin.right;
+    var height = 500 - margin.top - margin.bottom;
+    var y = d3.scale.linear()
+        .range([height, 0]);
+
+    var x = d3.time.scale()
+        .range([0, width]);
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left");
+
+    var line = d3.svg.line()
+        .x(function (d) {
+            return x(d.date);
+        })
+        .y(function (d) {
+            return y(d.steps);
+        });
+
+    d3.select("#steps .chart svg").remove();
+
+    var svg = d3.select("#steps .chart").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var data = window.data;
+
+    x.domain(d3.extent(data, function (d) {
+        return d.date;
+    }));
+    y.domain(d3.extent(data, function (d) {
+        return d.steps;
+    }));
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Steps");
+
+    svg.append("path")
+        .datum(data)
+        .attr("class", "line")
+        .attr("d", line);
+}
 
 window.fileSelection = function fileSelection(event) {
     window.data = [];
+    processCount = 0;
     var files = event.target.files;
+    fileCount = files.length;
     for (var x = 0; x < files.length; x++) {
         var reader = new FileReader();
         reader.onload = function (event) {
