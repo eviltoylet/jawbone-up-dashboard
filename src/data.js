2b59a2ba2a397b4f2cd69d5802f870ee08a596bc
Brian Chang <bchang85@hotmail.com>
2015-11-14 16:15:55 -0800

    Add line chart for number of steps

--- src/data.js
+++ src/data.js
@@ -1,5 +1,7 @@
 window.data = [];
 
+var parseDate = d3.time.format("%Y%m%d").parse;
+
 var processFileContents = function (content) {
     // underscore might be helpful here?
     var lines = content.split(/[\r\n]+/g);
@@ -15,17 +17,25 @@ var processFileContents = function (content) {
     }
 
     for (var x = 1; x < lines.length; x++) {
+        if (lines[x].trim().length == 0) {
+            continue;
+        }
         var lineData = lines[x].split(",");
+        var numericSteps = +lineData[stepIndex];
+        if (isNaN(numericSteps)) {
+            numericSteps = 0;
+        }
         window.data.push(
             {
-                date: lineData[dateIndex],
-                steps: lineData[stepIndex]
+                date: parseDate(lineData[dateIndex]),
+                steps: numericSteps
             }
         );
     }
 };
 
 window.fileSelection = function fileSelection(event) {
+    window.data = [];
     var files = event.target.files;
     for (var x = 0; x < files.length; x++) {
         var reader = new FileReader();
